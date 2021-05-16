4f3be70300c138bcd6e0586eeac7d855a341843a
taliev <taliev@luxoft.com>
2021-05-17 01:26:21 +0300

    feat: add product module/controller;

---
+++ src/product/product.module.ts
@@ -0,0 +1,8 @@
+import { Get, Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+
+@Module({
+  controllers: [ProductController]
+})
+export class ProductModule {
+}
